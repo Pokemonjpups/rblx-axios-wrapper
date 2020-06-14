f2ea08504af30c4c0960697065f067aa0ec1221f
buizel <beak@blockshub.net>
2020-06-14 14:49:29 -0400

    init :D

---
+++ index.d.ts
@@ -0,0 +1,192 @@
+import { AxiosInstance } from "axios";
+
+interface ExtendedAxiosInstance extends AxiosInstance {
+    /**
+     * If this is true, this client was setup by http.ts. Otherwise, it is likely a plain axios instance or something went wrong during the setup phase.
+     */
+    _setupByHttp: boolean;
+    /**
+     * A unique indentifer for the proxy used to organize the proxy pool.
+     */
+    id: string;
+
+    /**
+     * Get the Proxy Data related to the client.
+     * This will be false if the {ExtendedAxiosInstance} is not using a proxy.
+     */
+    getProxy: () => IProxyObject | false;
+
+    /**
+     * The ".ROBLOSECURITY" cookie
+     */
+    cookie?: string;
+}
+
+interface ExtendedAxiosInstanceWithCookie extends ExtendedAxiosInstance {
+    cookie: string;
+}
+
+/**
+ * A socks5 proxy object.
+ */
+interface IProxyObject {
+    /**
+     * The password of the proxy
+     */
+    proxyPassword?: string;
+    /**
+     * The username of the proxy
+     */
+    proxyLogin?: string;
+    /**
+     * The IP address of the proxy. Do not include the port.
+     */
+    proxyAddress: string;
+    /**
+     * The port of the proxy
+     */
+    proxyPort: string;
+}
+
+interface IClientOptions {
+    /**
+     * Only use proxy clients?
+     */
+    onlyProxy?: boolean;
+    /**
+     * Use a cookie from the cookie pool in the request?
+     */
+    useCookie?: boolean;
+    /**
+     * Use a proxy? Defaults to true
+     */
+    useProxy?: boolean;
+}
+
+// Enums
+/**
+ * The reason for why the HTTP client was marked as bad. Some reasons will disable the client forever, while others will re-check until it is alive again.
+ */
+export const ReasonForClientMarkedAsBad = {
+    'RequestTimeout': 1,
+    /**
+     * If the client just cannot be used whatsoever, use this to tell http.js not to re-add it to the pool
+     */
+    'DoNotReAddToPool': 2,
+}
+
+/**
+ * This exception is thrown when a method requires an agent with a proxy, but an agent without a proxy was provided.
+ */
+export class ClientIsNotAProxyException extends Error {}
+/**
+ * This exception is thrown when a proxy is required but no proxies are available
+ */
+export class NoProxiesAvailableException extends Error {}
+
+/**
+ * Register an array of proxies (or one proxy) into the proxy pool
+ * @param proxyArr 
+ */
+export function registerProxies(proxyArr: IProxyObject[] | IProxyObject): void;
+/**
+ * Get a Client from the proxy pool.
+ */
+// TODO: If options.useCookie === true, then return type is ExtendedAxiosInstanceWithCookie. Otherwise, return type is ExtendedAxiosInstance
+export function client<ClientOptions>(options?: IClientOptions): ExtendedAxiosInstance;
+
+/**
+ * Report a client as bad. This is normally handled automatically, but you may still need this method in some situations.
+ * @param client The client **MUST** be a proxy client (i.e. client.getProxy() must NOT return "false")
+ */
+export function badClient(client: ExtendedAxiosInstance, reason?: ReasonForClientMarkedAsBad): void;
+
+type MapDisableOption = "userId" | "csrf" | "all";
+/**
+ * Disable a map type. This can be useful if you run into memory overflow issues (when using thousands of cookies)
+ * @param option The map type to disable.
+ */
+export function disableMap(option: MapDisableOption): void;
+
+/**
+ * Set the provider for captcha tokens.
+ * 
+ * If you are using antiCaptcha, you can use the method returned from {http.captchaProvider.antiCaptcha()}. 
+ * 
+ * If you need to build a custom solution, the {provider} parameter must be a method that takes two arguments (the first being the public key, and the second being a proxy object). The function you make must return a promise that resolves with the funcaptcha token
+ * @param token 
+ */
+export function setCaptchaProvider(provider: (arg1: string, arg2: IProxyObject) => Promise<string>): void;
+
+/**
+ * Set the limit for captchas. This means that, there will never be more than {limit} number of captchas running at once. 
+ * 
+ * This can help prevent issues like a random surge in captcha requests (either intentional or accidental), preventing you from accidentally requesting 1000 captchas at once, however, the lower this limit is, the slower captcha solving will be.
+ * 
+ * The default limit is 10.
+ * @param limit 
+ */
+export function setCaptchaAsyncLimit(limit: number): void;
+
+/**
+ * This is useful for debugging dev stuff but should be kept disabled in production-type scenerios.
+ * 
+ * When set to true, if an e.code error is encountered (econnreset, etimedout, econnaborted, etc), the request will fail. If set to false, the request will be repeatedly retried until it succeeds. Defaults to false
+ * @param bool 
+ */
+export function failOnCodeErrors(bool: boolean): void;
+
+/**
+ * The default captcha providers included with this library.
+ */
+export namespace captchaProviders {
+    /**
+     * This is a provider method used for http.setCaptchaProvider()
+     * @param key Provide your antiCaptcha private key here
+     */
+    export function antiCaptcha(key: string): (publicKey: string, proxyData: IProxyObject) => Promise<string>
+
+    /**
+     * Set the max solve time for the default captcha providers (in MS). 
+     * 
+     * **You should probably *not* set this unless you are specifically OK with requests failing due to slow captcha solving times (note: most captcha solving providers will still charge for timed out captchas).**
+     * 
+     * Defaults to Number.MAX_SAFE_INTEGER - 1
+     * @param ms 
+     */
+    export function setMaxSolveTime(ms: number): void;
+}
+
+/**
+ * Cookie pooling library. Useful for botting purposes
+ */
+export namespace cookie {
+    /**
+     * This exception is thrown when a method requiring at least one cookie in the pool is called when the pool is empty
+     */
+    export class EmptyPoolException extends Error {};
+    /**
+     * Add a cooke (or array of cookies) to the cookie pool
+     * 
+     * If the cookie is prefixed with ".ROBLOESECURITY=", that part will be sliced off
+     * @param cookie 
+     */
+    export function add(cookie: string | string[]): void;
+
+    /**
+     * Validate all cookies in the cookie pool and remove invalid ones. 
+     * 
+     * This will also set the cookieToId map objects
+     */
+    export function validatePool(): Promise<void>;
+
+    /**
+     * Get a random cookie from the pool
+     */
+    export function get(): string;
+
+    /**
+     * Get a mutatable object containing the pool of cookies
+     */
+    export function getPool(): Set<string>;
+}
---
+++ index.js
@@ -0,0 +1,476 @@
+/// <reference path="index.d.ts" />
+let _proxyAgents = [];
+/**
+ * @type {import('axios').AxiosInstance}
+ */
+let _normalAgent;
+
+const SocksProxyAgent = require('socks-proxy-agent');
+const axiosLib = require('axios').default;
+
+const sleep = require('util').promisify(setTimeout);
+const _ = require('lodash');
+
+let _captchaProvider = undefined;
+let _maxPendingCaptchasAtOnce = 10;
+let _pendingCaptchas = 0;
+let publicKeys = [];
+const getPublicKey = async (key) => {
+    await _firstCaptchaSetupRequest;
+    for (const item of publicKeys) {
+        if (item.type === key) {
+            return item.value;
+        } 
+    }
+}
+
+let _disableCookieToCsrfMap = false;
+/**
+ * @type {Map<string, string>}
+ */
+const cookieToCsrfMap = new Map();
+
+let _disableCookieToUserIdMap = false;
+/**
+ * @type {Map<string, number>}
+ */
+const cookieToUserIdMap = new Map();
+
+let _failOnCodeErrors = false;
+
+/**
+ * Setup an axiosinstance with the proper csrf/http code intersceptors
+ * @param {import('axios').AxiosInstance} proxy 
+ */
+const setupIntersceptors = (proxy) => {
+    //@ts-ignore
+    if (proxy._setupByHttp) {
+        return;
+    }
+    // @ts-ignore
+    proxy._setupByHttp = true;
+    // @ts-ignore
+    proxy.id = require('crypto').randomBytes(32).toString('hex');
+
+    let _lastUsedOkCsrf = 'null';
+    proxy.interceptors.request.use(conf => {
+        // @ts-ignore
+        if (proxy.cookie) {
+            if (typeof conf.headers['cookie'] !== 'string') {
+                conf.headers['cookie'] = '';
+            }
+            // confirm we arent sending two cookies
+            if (!conf.headers.cookie.match(/\.ROBLOSECURITY/g)) {
+                // @ts-ignore
+                conf.headers.cookie = '.ROBLOSECURITY='+proxy.cookie+'; '+conf.headers.cookie;
+            }
+        }
+        if (!conf.headers['x-csrf-token']) {
+            if (!_disableCookieToCsrfMap) {
+                if (conf.headers['cookie'] && conf.headers.cookie.match(/\.ROBLOSECURITY=/g)) {
+                    let _otherCsrfKey = conf.headers.cookie.match(/\.ROBLSECURITY=(.+?)(;|)/g);
+                    let _otherCsrf = cookieToCsrfMap.get(_otherCsrfKey);
+                    if (_otherCsrf) {
+                        _lastUsedOkCsrf = _otherCsrf;
+                    }
+                    
+                }
+            }
+            conf.headers['x-csrf-token'] = _lastUsedOkCsrf;
+        }
+        return conf;
+    })
+    proxy.interceptors.response.use(undefined, err => {
+
+        const captchaErrorTypes = [
+            // Login v1/v2
+            {
+                urls: [
+                    /^https:\/\/auth.roblox.com\/v(1|2)\/login$/g
+                ],
+                code: 2,
+                message: 'You must pass the robot test before logging in.',
+                keyName: 'WebLogin',
+            },
+            // Signup v1/v2/v3
+            {
+                urls: [
+                    /^https:\/\/auth.roblox.com\/v(1|2|3)\/signup$/,
+                ],
+                code: 2,
+                message: 'Captcha Failed.',
+                keyName: 'WebSignup',
+            },
+            // Group join v1
+            {
+                urls: [
+                    /^https:\/\/groups.roblox.com\/v1\/groups\/(\d+?)\/users$/,
+                ],
+                code: 5,
+                message: 'You must pass the captcha test before joining this group.',
+                keyName: 'UserAction',
+            },
+            // friend request v1
+            {
+                urls: [
+                    /^https:\/\/friends.roblox.com\/v1\/users\/(\d+?)\/request-friendship$/,
+                ],
+                code: 14,
+                message: 'The user has not passed the captcha.',
+                keyName: 'UserAction',
+            },
+            // follow request v1
+            {
+                urls: [
+                    /^https:\/\/friends.roblox.com\/v1\/users\/(\d+?)\/follow$/,
+                ],
+                code: 14,
+                message: 'The user has not passed the captcha.',
+                keyName: 'UserAction',
+            }
+        ]
+
+        /**
+        * @type {import('axios').AxiosError<{errors: any[]}>}
+        */
+        let e = err;
+        if (e.config.data && typeof e.config.data === 'string') {
+            e.config.data = JSON.parse(e.config.data);
+        }
+        if (err && err.response) {
+            if (err.response.status === 429) {
+                return sleep(5000).then(() => {
+                    return proxy.request(e.config)
+                });
+            }else if (err.response.status === 403) {
+                for (const item of e.response.data.errors) {
+                    for (const type of captchaErrorTypes) {
+                        let _isMatch = false;
+                        for (const urlType of type.urls) {
+                            let _matchUrl = e.config.url;
+                            if (_matchUrl.slice(_matchUrl.length-1) === '/') {
+                                _matchUrl = _matchUrl.slice(0,_matchUrl.length-1);
+                            }
+                            if (_matchUrl.match(urlType)) {
+                                _isMatch = true;
+                            }
+                        }
+                        if (_isMatch) {
+                            if (item.code === type.code && item.message.toLowerCase() === type.message.toLowerCase()) {
+                                e.config.data['captchaProvider'] = 'PROVIDER_ARKOSE_LABS';
+                                // temporary
+                                if (_pendingCaptchas >= _maxPendingCaptchasAtOnce) {
+                                    throw new Error('[temporary] Too many captchas at once.');
+                                }
+                                _pendingCaptchas++;
+                                return getPublicKey(type.keyName).then(loginKey => {
+                                    // @ts-ignore
+                                    return _captchaProvider(loginKey, proxy.getProxy()).then(captchaToken => {
+                                        _pendingCaptchas--;
+                                        e.config.data['captchaToken'] = captchaToken;
+                                        return proxy.request(e.config)
+                                    }).catch(err => {
+                                        _pendingCaptchas--;
+                                        throw err;
+                                    })
+                                })
+                            }
+                        }
+                    }
+                }
+                let _csrf = err.response.headers['x-csrf-token'];
+                if (_csrf) {
+                    err.config.headers['x-csrf-token'] = _csrf;
+                    _lastUsedOkCsrf = _csrf;
+                    if (!_disableCookieToCsrfMap && err.config.headers.cookie) {
+                        let _otherCsrfKey = err.config.headers.cookie.match(/\.ROBLSECURITY=(.+?)(;|)/g);
+                        if (_otherCsrfKey) {
+                            cookieToCsrfMap.set(_otherCsrfKey, _csrf);
+                        }
+                    }
+                    return proxy.request(err.config);
+                }
+            }
+        }
+        return Promise.reject(err);
+    });
+}
+
+(() => {
+    const generalAxiosAgent = axiosLib.create({});
+    setupIntersceptors(generalAxiosAgent);
+    // @ts-ignore
+    generalAxiosAgent.getProxy = () => {
+        return false;
+    }
+    _normalAgent = generalAxiosAgent;
+})();
+
+let _maxCaptchaSolveTime = Number.MAX_SAFE_INTEGER - 1;
+const captchaProviders = {
+    _userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4143.7 Safari/537.36',
+    setUserAgent: (str) => {
+        captchaProviders._userAgent = str;
+    },
+    setMaxSolveTime: (maxTime) => {
+        _maxCaptchaSolveTime = maxTime;
+    },
+    antiCaptcha: (key, debug = false) => {
+        const {
+            AntiCaptcha,
+            AntiCaptchaError,
+            TaskTypes,
+            ErrorCodes,
+        } = require("anticaptcha");
+        const AntiCaptchaAPI = new AntiCaptcha(key, debug); // You can pass true as second argument to enable debug logs.
+        const antiCaptchaProvider = async (publicKey, proxyObject) => {
+            // create task
+            const taskId = await AntiCaptchaAPI.createTask({
+                type: TaskTypes.FUN_CAPTCHA,
+                websiteURL: 'https://www.roblox.com',
+                websitePublicKey: publicKey,
+                // funcaptchaApiJSSubdomain: '',
+                proxyType: 'socks5',
+                proxyAddress: proxyObject.proxyAddress,
+                proxyPort: proxyObject.proxyPort,
+                proxyLogin: proxyObject.proxyLogin,
+                proxyPassword: proxyObject.proxyPassword,
+                userAgent: captchaProviders._userAgent,
+            });
+
+            // Waiting for resolution and do something
+            const response = await AntiCaptchaAPI.getTaskResult(taskId, _maxCaptchaSolveTime);
+            let token = response.solution.token;
+            return token;
+        }
+        return antiCaptchaProvider;
+    }
+}
+
+/**
+ * @type {Set<string>}
+ */
+let _cookieArray = new Set();
+
+let _randomCookieIndex = 0;
+const cookie = {
+    EmptyPoolException: class extends Error {},
+    add: (val) => {
+        if (!Array.isArray(val)) {
+            let cookieItem = val;
+            val = [];
+            val.push(cookieItem);
+        }
+
+        for (let cookie of val) {
+            if (cookie.slice(0,'.ROBLOSECURITY='.length) === '.ROBLOSECURITY=') {
+                cookie = cookie.slice('.ROBLOSECURITY='.length);
+            }
+            _cookieArray.add(cookie);
+        }
+    },
+    validatePool: async () => {
+        let cookieChunk = _.chunk(Array.from(_cookieArray), 100);
+        for (const chunk of cookieChunk) {
+            let proms = [];
+            for (const cookie of chunk) {
+                const handleCookie = async (cookie) => {
+                    let cl = http.client({
+                        useCookie: false,
+                    });
+                    cl.cookie = cookie;
+                    try {
+                        let res = await cl.get('https://users.roblox.com/v1/users/authenticated');
+                        let id = res.data.id;
+                        cookieToUserIdMap.set(cookie, id);
+                    }catch(err) {
+                        // console.log(err);
+                        // todo: analyize performance implications of this
+                        _cookieArray = new Set(Array.from(_cookieArray).filter(val => {
+                            return val !== cookie;
+                        }));
+                    }
+                }
+                proms.push(handleCookie(cookie));
+            }
+            await Promise.all(proms);
+        }
+    },
+    get: () => {
+        let _cookieSetAsArray = Array.from(_cookieArray);
+        let cookieToGrab = _cookieSetAsArray[_randomCookieIndex];
+        if (typeof cookieToGrab !== 'string') {
+            _randomCookieIndex = 0;
+            let firstVal = _cookieSetAsArray[0];
+            if (typeof firstVal !== 'string') {
+                throw new cookie.EmptyPoolException('The cookie pool is empty! Register cookies with cookie.add()');
+            }
+            return firstVal;
+        }else{
+            _randomCookieIndex++;
+            return cookieToGrab;
+        }
+    },
+    getPool: () => {
+        return _cookieArray;
+    },
+}
+
+let _randomAxiosIndex = 0;
+const http = {
+    // Errors
+    ClientIsNotAProxyException: class extends Error { },
+    NoProxiesAvailableException: class extends Error { },
+    // Enums
+    ReasonForClientMarkedAsBad: {
+        'RequestTimeout': 1,
+        'DoNotReAddToPool': 2,
+    },
+    // Extra Objects
+    captchaProviders: captchaProviders,
+    // Cookie class
+    cookie: cookie,
+    // Methods
+    registerProxies: (proxyArr) => {
+        if (!Array.isArray(proxyArr)) {
+            let agentToInsert = proxyArr;
+            proxyArr = [];
+            proxyArr.push(agentToInsert);
+        }
+        for (const proxy of proxyArr) {
+            const httpsAgent = SocksProxyAgent({
+                password: proxy.proxyPassword,
+                userId: proxy.proxyLogin,
+                host: proxy.proxyAddress,
+                port: proxy.proxyPort,
+            });
+            const axios = axiosLib.create({
+                httpsAgent: httpsAgent,
+            });
+            setupIntersceptors(axios);
+            // @ts-ignore
+            axios.getProxy = () => {
+                return proxy;
+            }
+            _proxyAgents.push(axios);
+        }
+    },
+    client: (options = undefined) => {
+        // Grab an agent from the proxy pool
+        /**
+         * @type {import('axios').AxiosInstance}
+         */
+        let axiosInstanceToGrab = _proxyAgents[_randomAxiosIndex];
+        if (options) {
+            if (options.useProxy === false) {
+                axiosInstanceToGrab = _normalAgent;
+            }
+            if (options.onlyProxy && _proxyAgents.length === 0) {
+                throw new http.NoProxiesAvailableException('Proxy pool is empty. Please register proxies with http.registerProxies()');
+            }
+        }
+        if (!axiosInstanceToGrab) {
+            _randomAxiosIndex = 0;
+            let first = _proxyAgents[0];
+            if (!first) {
+                axiosInstanceToGrab = _normalAgent;
+            } else {
+                axiosInstanceToGrab = first;
+            }
+        } else {
+            _randomAxiosIndex++;
+        }
+        // register an intersceptor
+        axiosInstanceToGrab.interceptors.response.use(undefined, _err => {
+            /**
+             * @type {import('axios').AxiosError}
+             */
+            let e = _err;
+            let msg = e.message.toLowerCase();
+            // Check if there is an issue with the proxy
+            if (msg.indexOf('socks5') !== -1 || msg.indexOf('proxy') !== -1) {
+                // The proxy is bad. Put it on a timeout, then try again with a new client
+                http.badClient(axiosInstanceToGrab);
+                return http.client(options).request(_err.config);
+            }
+            // Check if there is an issue that cannot be solved by the library
+            if (e.code && !_failOnCodeErrors) {
+                // auto-retry until OK
+                return axiosInstanceToGrab.request(e.config);
+            }
+
+            return Promise.reject(e);
+        })
+        if (options && options.useCookie) {
+            axiosInstanceToGrab.defaults.headers['cookie'] = '.ROBLOSECURITY='+cookie.get();
+        }
+        return axiosInstanceToGrab;
+    },
+    badClient: (client, reason) => {
+        if (!client.getProxy()) {
+            // Not a proxy
+            throw new http.ClientIsNotAProxyException('Non-proxy clients cannot be marked as bad.');
+        } else {
+            let id = client.id;
+            _proxyAgents = _proxyAgents.filter(el => {
+                return el.id !== id;
+            });
+            if (reason === http.ReasonForClientMarkedAsBad.DoNotReAddToPool) {
+                return; // dont try re-adding
+            }
+            let _clientRequestPending = false;
+            let _retryInterval = setInterval(() => {
+                // small debounce check incase timeout
+                if (_clientRequestPending) {
+                    return;
+                }
+                // See if the client is OK
+                client.get('https://www.roblox.com/robots.txt').then(res => {
+                    if (res && res.status && res.status === 200) {
+                        // Client seems to be OK
+                        _proxyAgents.push(client);
+                        clearInterval(_retryInterval);
+                    }
+                }).catch(err => {
+                    // Client is still bad.
+                }).finally(() => {
+                    _clientRequestPending = false;
+                });
+            }, 60000);
+        }
+    },
+    disableMap: (option) => {
+        if (option === 'csrf') {
+            _disableCookieToCsrfMap = true;
+        }else if (option === 'userId') {
+            _disableCookieToUserIdMap = true;
+        }else if (option === 'all') {
+            _disableCookieToCsrfMap = true;
+            _disableCookieToUserIdMap = true;
+        }
+    },
+    failOnCodeErrors: (bool) => {
+        _failOnCodeErrors = bool;
+    },
+    // Captcha methods
+    setCaptchaProvider: (provider) => {
+        _captchaProvider = provider;
+    },
+    setCaptchaAsyncLimit: (limit) => {
+        _maxPendingCaptchasAtOnce = limit;
+    },
+}
+module.exports = http;
+
+// update captcha stuff
+let _firstCaptchaSetupRequest = http.client().get('https://captcha.roblox.com/v1/captcha/metadata').then(res => {
+    publicKeys = res.data.funCaptchaPublicKeys;
+})
+setInterval(() => {
+    // update captcha stuff
+    http.client({}).get('https://captcha.roblox.com/v1/captcha/metadata').then(res => {
+        publicKeys = res.data.funCaptchaPublicKeys;
+    }).catch(err => {
+        console.error(err);
+    })
+}, 60 * 1000);
