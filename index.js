8c79fba0ee49649296f77809b52c1522597fd0e9
buizel <27046219+Pokemonjpups@users.noreply.github.com>
2020-11-23 16:50:25 -0500

    [add] cached dns resolve

--- index.js
+++ index.js
@@ -58,6 +58,8 @@ const cookieToUserIdMap = new Map();
 
 let _failOnCodeErrors = false;
 
+const { registerInterceptor } = require('axios-cached-dns-resolve');
+
 const readFileAsync = util.promisify(fs.readFile);
 /**
  * Setup an axiosinstance with the proper csrf/http code intersceptors
@@ -73,6 +75,7 @@ const setupInterceptors = (proxy) => {
     proxy._setupByHttp = true;
     // @ts-ignore
     proxy.id = crypto.randomBytes(32).toString('hex');
+    registerInterceptor(proxy);
 
     let _lastUsedOkCsrf = 'null';
     proxy.interceptors.request.use(conf => {
